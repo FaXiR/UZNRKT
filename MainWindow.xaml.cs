9493fad5f1c5600377af4b4f7837d9262a81f484
FaXiR <fFaXiR@yandex.ru>
2020-03-12 20:52:33 +0500

    Корректировка вывода в эксель (Сохранение теперь из Excel приложения)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -525,14 +525,6 @@ namespace UZNRKT
         /// </summary>
         private void OutToExcell(string title, DataView table)
         {
-            SaveFileDialog sv = new SaveFileDialog();
-            sv.Filter = "Excel файлы|*.xlsx|Все файлы|*.*";
-            if (sv.ShowDialog() == false)
-            {
-                return;
-            }
-            string filename = sv.FileName;
-
             var excelapp = new Microsoft.Office.Interop.Excel.Application();
             var workbook = excelapp.Workbooks.Add();
             Microsoft.Office.Interop.Excel.Worksheet worksheet = workbook.ActiveSheet;
@@ -543,11 +535,13 @@ namespace UZNRKT
             {
                 ColumnName.Add(table.Table.Columns[i].ToString());
             }
+
             //Выводим название колонок
             for (int x = 0; x < ColumnName.Count; x++)
             {
                 worksheet.Rows[2].Columns[x + 1] = ColumnName[x];
             }
+
             //заполням ячейки
             for (int y = 3; y < table.Count + 3; y++)
             {
@@ -557,35 +551,40 @@ namespace UZNRKT
                 }
             }
 
-            // Выделяем диапазон ячеек от A1 до нужной         
-            Microsoft.Office.Interop.Excel.Range _excelCells1 = (Microsoft.Office.Interop.Excel.Range)worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1], (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, ColumnName.Count]).Cells;
-            // Производим объединение
-            _excelCells1.Merge(Type.Missing);
-            //Задаем титульник
-            worksheet.Cells[1, 1] = title;
-            //Размер текста
-            _excelCells1.Cells.Font.Size = 16;
-            //Выравнивание
-            _excelCells1.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
-            //Задание bold для текста
-            _excelCells1.Font.Bold = true;
+            // (Титульник над содержимым) Выделяем диапазон ячеек от A1 до числа столбцов из DataView       
+            Microsoft.Office.Interop.Excel.Range TitleRange = (Microsoft.Office.Interop.Excel.Range)worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1], (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, ColumnName.Count]).Cells;
 
-            //Выделение всех ячеек
-            Microsoft.Office.Interop.Excel.Range _excelCells2 = (Microsoft.Office.Interop.Excel.Range)worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1], (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[table.Count + 2, ColumnName.Count]).Cells;
+            // Производим объединение
+            TitleRange.Merge(Type.Missing);
+            
+            //Размер текста
+            TitleRange.Cells.Font.Size = 16;
+            
+            //Выравнивание по центру
+            TitleRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            
+            //Задание bold для текста
+            TitleRange.Font.Bold = true;
+
+            //Задаем название титульника
+            worksheet.Cells[1, 1] = title;
+
+            //Выделение всех ячеек с данными
+            Microsoft.Office.Interop.Excel.Range ContentRange = (Microsoft.Office.Interop.Excel.Range)worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1], (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[table.Count + 2, ColumnName.Count]).Cells;
 
             //Выставление линий 
-            _excelCells2.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
-            _excelCells2.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeRight).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
-            _excelCells2.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideHorizontal).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
-            _excelCells2.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideVertical).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
-            _excelCells2.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            ContentRange.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            ContentRange.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeRight).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            ContentRange.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideHorizontal).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            ContentRange.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideVertical).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            ContentRange.Borders.get_Item(Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop).LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
 
+            //Выставление автоширины
+            ContentRange.EntireColumn.AutoFit();
+
+            //Отображаем Excel
             excelapp.AlertBeforeOverwriting = false;
-            workbook.SaveAs(filename);
-            excelapp.Quit();
-
-            //TODO: запускаем
-            Process.Start(filename);
+            excelapp.Visible = true;
         }
     }
 }
